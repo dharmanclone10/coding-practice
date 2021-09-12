d31b94c01e29480ef7ea0d8180c01af714fc5a47
dharmanclone10 <90488883+dharmanclone10@users.noreply.github.com>
2021-09-12 04:55:44 -0500

    challange 5

--- src/Main.hx
+++ src/Main.hx
@@ -4,7 +4,7 @@ class Main
 {
 	public static var splitGrps:String;
 
-	public static var nextMessage:Array<String> = ['missions/mission$mission'];
+	public static var nextMessage:Array<String> = ['src/missions/mission$mission'];
 
 	public static function missions()
 	{
@@ -15,40 +15,47 @@ class Main
 		for (splitGrps in missions)
 		{
 			nextMessage.split();
-			
+
 			forEach(nextMessage);
 			spliting(nextMessage);
 
 			// var string:String;
 			numPad.pad();
-			
+
 			trace(nextMessage);
 		}
 
 		for (splitInNumbers in nextMessage)
 		{
 			nextMessage.forEach(splitInNumbers);
+			forEachMessage(output[1]);
 			trace(nextMessage);
 		}
 
 		function splitWordSideways()
 		{
-			for (i in pairs, nextMessage)
+			// splits into 2's
+			pie(5);
+			splits(2);
+
+			split(pie);
+			pairs.split('-- ---');
+
+			nextMessage.pie();
+			trace(pie, + nextMessage);
+
+			secondPie(3);
+			splitGrps(1);
+
+			trace(splitWordSideways);
+		}
+
+		function mathProblems()
+		{
+			for (math in i...num + secondNum)
 			{
-				// splits into 2's
-				pie(5);
-				splits(2);
-				
-				split(pie);
-				// pairs.split('-- ---');
-
-				nextMessage.pie();
-				trace(pie, + nextMessage);
-
-				secondPie(3);
-				splitGrps(1);
-				
-				trace(splitWordSideways);
+				Sys.output(19);
+				trace(mathProblems);
 			}
 		}
 	}
