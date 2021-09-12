bf4b389335876f4e976b9a1cdb8e3ec4986b8a6b
dharmanclone10 <90488883+dharmanclone10@users.noreply.github.com>
2021-09-12 04:43:11 -0500

    coding lol!

---
+++ src/Main.hx
@@ -0,0 +1,55 @@
+package;
+
+class Main
+{
+	public static var splitGrps:String;
+
+	public static var nextMessage:Array<String> = ['missions/mission$mission'];
+
+	public static function missions()
+	{
+		public static var missions:String;
+
+		nextMessage(forEach);
+
+		for (splitGrps in missions)
+		{
+			nextMessage.split();
+			
+			forEach(nextMessage);
+			spliting(nextMessage);
+
+			// var string:String;
+			numPad.pad();
+			
+			trace(nextMessage);
+		}
+
+		for (splitInNumbers in nextMessage)
+		{
+			nextMessage.forEach(splitInNumbers);
+			trace(nextMessage);
+		}
+
+		function splitWordSideways()
+		{
+			for (i in pairs, nextMessage)
+			{
+				// splits into 2's
+				pie(5);
+				splits(2);
+				
+				split(pie);
+				// pairs.split('-- ---');
+
+				nextMessage.pie();
+				trace(pie, + nextMessage);
+
+				secondPie(3);
+				splitGrps(1);
+				
+				trace(splitWordSideways);
+			}
+		}
+	}
+}
